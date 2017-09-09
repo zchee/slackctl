eaf76558fd62956187c8a69636b9597007239f56
Koichi Shiraishi <zchee.io@gmail.com>
2017-09-09 22:35:09 +0900

    all: add Channels function
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ channel.go
@@ -0,0 +1,42 @@
+// Copyright 2017 The slackctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package slackctl
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"text/tabwriter"
+
+	slack "github.com/lestrrat/go-slack"
+)
+
+func Channels() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client := slack.New(token)
+	authresp, err := client.Auth().Test().Do(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "Team: %s\n", authresp.Team)
+
+	channels, err := client.Channels().List().ExclArchived(true).Do(ctx)
+	if err != nil {
+		return err
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 1, 8, 1, '\t', 0)
+	for _, ch := range channels {
+		tw.Write([]byte(fmt.Sprintf("%s\t%s\n", ch.Name, strconv.Itoa(len(ch.Members)))))
+	}
+	if err := tw.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
